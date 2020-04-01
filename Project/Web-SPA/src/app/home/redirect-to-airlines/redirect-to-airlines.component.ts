744076b52cb5cf0f1ca5fc2c21bd49787c733745
Milos <milosbakmaz5@gmail.com>
2020-04-01 02:04:28 +0200

    Izmene u dizajnu.

--- Project/Web-SPA/src/app/home/redirect-to-airlines/redirect-to-airlines.component.ts
+++ Project/Web-SPA/src/app/home/redirect-to-airlines/redirect-to-airlines.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-redirect-to-airlines',
   templateUrl: './redirect-to-airlines.component.html',
-  styleUrls: ['./redirect-to-airlines.component.scss', '../redirect-to-cars/redirect-to-cars.component.scss']
+  styleUrls: ['./redirect-to-airlines.component.scss']
 })
 export class RedirectToAirlinesComponent implements OnInit {
 
